3d557e60630a7750bd17bba741d61a3b24c3b43e
muthu-PM <muthu@abc.com>
2025-01-30 22:42:22 +0530

    post-put-delete code added

--- Routes/ProductRoutes.js
+++ Routes/ProductRoutes.js
@@ -4,6 +4,9 @@ const router = require('express').Router()
 
 router.get('/api/products', productController.getAllProducts);
 router.get('/api/products/:id', productController.getSingleProduct)
+router.post('/api/product', productController.addNewProduct)
+router.put('/api/product', productController.updateExistProduct)
+router.delete('/api/product/:id', productController.deleteProduct)
 
 module.exports = router
 
